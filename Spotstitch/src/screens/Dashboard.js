36dafe0f7f3187f224d0efddb5ca94cb063f231c
SHaiderX <ShaiderHB@gmail.com>
2023-02-17 10:45:25 -0500

    Dashboard - Tabs, Posts, Post Screen

--- Spotstitch/src/screens/Dashboard.js
+++ Spotstitch/src/screens/Dashboard.js
@@ -1,30 +1,70 @@
-import React from 'react'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Paragraph from '../components/Paragraph'
-import Button from '../components/Button'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Post from '../components/Post';
 
-export default function Dashboard({ navigation }) {
-  return (
-    <Background>
-      <Logo />
-      <Header>Let’s start</Header>
-      <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
-      </Paragraph>
-      <Button
-        mode="outlined"
-        onPress={() =>
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'StartScreen' }],
-          })
-        }
-      >
-        Logout
-      </Button>
-    </Background>
-  )
-}
+const Tab = createMaterialTopTabNavigator();
+
+const posts = [
+  {
+    id: 1,
+    username: 'John Doe',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum dolor in orci convallis, eu aliquet dui congue.',
+    type: 'personal',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg'
+  },
+  {
+    id: 2,
+    username: 'Jane Doe',
+    text: 'Sed placerat metus id metus dapibus, a posuere lacus hendrerit.',
+    type: 'events',
+  },
+  {
+    id: 3,
+    username: 'John Doe',
+    text: 'Vestibulum vel sapien vel velit facilisis aliquam.',
+    type: 'personal',
+  },
+  {
+    id: 4,
+    username: 'Jane Doe',
+    text: 'Nam interdum risus in diam laoreet, id commodo risus tincidunt.',
+    type: 'events',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg'
+  },
+];
+
+const MyPostsScreen = () => (
+  <View style={styles.container}>
+    {posts
+      .filter((post) => post.type === 'personal')
+      .map((post) => (
+        <Post key={post.id} username={post.username} caption={post.text} image={post.image} />
+      ))}
+  </View>
+);
+
+const FavoritesScreen = () => (
+  <View style={styles.container}>
+    {posts
+      .filter((post) => post.type === 'events')
+      .map((post) => (
+        <Post key={post.id} username={post.username} caption={post.text} image={post.image} />
+      ))}
+  </View>
+);
+
+const Dashboard = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="My Posts" component={MyPostsScreen} />
+    <Tab.Screen name="Favorites" component={FavoritesScreen} />
+  </Tab.Navigator>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Dashboard;
